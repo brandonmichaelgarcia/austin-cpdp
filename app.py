0f7257bbf8a44f4911502c41f43ab5766cc5fdbb
Brandon Michael Garcia <brandon.michael.garcia@gmail.com>
2018-03-06 18:12:00 -0600

    Added visualizations and basic endpoints other than index.

--- app.py
+++ app.py
@@ -11,6 +11,18 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 
 if __name__ == '__main__':
--- static/chartvis.js
+++ static/chartvis.js
@@ -1,132 +1,236 @@
-d3.csv("/static/data/us-income-inequality.csv", extract_data);
-var [label_array, val_array, maxval, sampsize] = [0,0,0,0];
+var Chart = (function(window,d3) {
 
+    var svg, data, x, y, xAxis, yAxis, dim, chartWrapper, line, path, margin = {}, width, height;
+    var breakPoint = 600;
+    var chart_id = 0;
+    d3.csv('/static/data/ois.csv', init); //load data, then initialize chart
 
-function drawLoop(data){
-    [label_array, val_array, maxval, sampsize] = data;
+    //called once the data is loaded
+    function init(csv) {
+        data = csv;
 
-    draw();
+        //initialize scales
+        xExtent = d3.extent(data, function(d,i) { return new Date(d.date) });
+        yExtent = d3.extent(data, function(d,i) { return d.value });
+        zExtent = d3.extent(data, function(d,i) { return d.value2 });
+        x = d3.time.scale().domain(xExtent).nice();
+        y = d3.scale.linear().domain(yExtent);
+        z = d3.scale.linear().domain(zExtent);
 
-    d3.select(window).on("resize", draw);
-}
+        //initialize axis
+        xAxis = d3.svg.axis().orient('bottom');
+        yAxis = d3.svg.axis();
+        zAxis = d3.svg.axis();
 
+        //the path generator for the line chart
+        line = d3.svg.line()
+            .x(function(d) { return x(new Date(d.date)) })
+            .y(function(d) { return y(d.value) });  
+        
+        line2 = d3.svg.line()
+            .x(function(d) { return x(new Date(d.date)) })
+            .y(function(d) { return z(d.value2) }); 
 
-function draw() {
-    d3.select('#line-chart').selectAll('*').remove();
-    var width = (window.innerWidth > 600) ? 900 : 400;
-    var p = 30;
-    var  w = width - p*4;
-    var  h = 500;
+        //initialize svg
+        svg = d3.select('#chart').append('svg');
+        chartWrapper = svg.append('g');
+        path = chartWrapper.append('path').datum(data).classed('line', true);
+        chartWrapper.append('g').classed('x axis', true);
+        chartWrapper.append('g').classed('y axis', true);
+        chartWrapper.on('mousemove', identifyMouse)
+        
+        
+        //add locator and text
+        locator = chartWrapper.append('circle')
+            .attr('r', 10)
+            .attr('stroke', 'steelblue')
+            .attr('stroke-width', 2)
+            .attr('fill', 'none')
+            .attr('display', 'none')
 
-    var x = d3.scale.linear()
-                .domain([ label_array[0], label_array[sampsize-1] ])
-                .range([0, w]);
-    var y = d3.scale.linear()
-                .domain([0, maxval])
-                .range([h, 0]);
+        ordinate_label = d3.select("body").append("div")	
+            .attr("class", "tooltip")				
+            .style("opacity", 0);
 
-    var vis = d3.select("#line-chart")
-                .data([val_array])
-                .append("svg:svg")
-                .attr("width", w + p *2)
-                .attr("height", h + p * 2)
-                .append("svg:g")
-                .attr("transform", "translate(" + p + "," + p + ")");
+        bisect = d3.bisector(function(d){ return d.date; }).left;
 
-
-    var rules = vis.selectAll("g.rule")
-                .data(x.ticks(15))
-                .enter().append("svg:g")
-                .attr("class", "rule");
-
-    // Draw grid lines
-    rules.append("svg:line")
-                .attr("x1", x)
-                .attr("x2", x)
-                .attr("y1", 0)
-                .attr("y2", h - 1);
-
-    rules.append("svg:line")
-                .attr("class", function(d) { return d ? null : "axis"; })
-                .data(y.ticks(10))
-                .attr("y1", y)
-                .attr("y2", y)
-                .attr("x1", 0)
-                .attr("x2", w);
-;        // Place axis tick labels
-    rules.append("svg:text")
-                .attr("x", x)
-                .attr("y", h + 15)
-                .attr("dy", ".71em")
-                .attr("text-anchor", "middle")
-                .text(x.tickFormat(10))
-                .text(String);
-
-    rules.append("svg:text")
-                .data(y.ticks(12))
-                .attr("y", y)
-                .attr("x", -10)
-                .attr("dy", ".35em")
-                .attr("text-anchor", "end")
-                .text(y.tickFormat(5));
-
-    // Series I
-    vis.append("svg:path")
-                .attr("class", "line")
-                .attr("fill", "none")
-                .attr("stroke", "steelblue")
-                .attr("stroke-width", 2)
-                .attr("d", d3.svg.line()
-                .x(function(d) { return x(d.x); })
-                .y(function(d) { return y(d.y); }));
-
-    vis.selectAll("circle.line")
-                .data(val_array)
-                .enter().append("svg:circle")
-                .attr("class", "line")
-                .attr("fill", "steelblue" )
-                .attr("cx", function(d) { return x(d.x); })
-                .attr("cy", function(d) { return y(d.y); })
-                .attr("r", 1);
-
-    // -----------------------------
-    // Add Title then Legend
-    // -----------------------------
-    vis.append("svg:text")
-                .attr("x", w/4)
-                .attr("y", 20)
-                .text("% share of income (excluding capital gains): U.S. 1920-2008");
-
-    vis.append("svg:rect")
-                .attr("x", w/2 - 20)
-                .attr("y", 50)
-                .attr("stroke", "steelblue")
-                .attr("height", 1)
-                .attr("width", 40);
-
-    vis.append("svg:text")
-                .attr("x", 30 + w/2)
-                .attr("y", 55)
-                .text("Top 5% households");
-}
-
-
-function extract_data(data1) { 
-    /* Read CSV file: first row =>  year,top1,top5  */
-    var maxval = 0;
-    var sampsize = 0;
-    var label_array = new Array();
-    var val_array = new Array();
-
-    sampsize = data1.length;
-
-    for (var i=0; i < sampsize; i++) {
-    label_array[i] = parseInt(data1[i].year);
-    val_array[i] = { x: label_array[i], y: parseFloat(data1[i].p99), z: parseFloat(data1[i].p95) };
-    maxval = Math.max(maxval, parseFloat(data1[i].p99), parseFloat(data1[i].p95) );
+        //render the chart
+        window.addEventListener('resize', render);
+        render();
     }
 
-    maxval = (1 + Math.floor(maxval / 10)) * 10;   
+    function render() {
+        updateDimensions(window.innerWidth);
+        
+        //update x and y scales to new dimensions
+        x.range([0, width]);
+        y.range([height,0]);
+        z.range([height, 0]);
 
-    drawLoop([label_array, val_array, maxval, sampsize]);
-}
+        //update svg elements to new dimensions
+        svg
+          .attr('width', width + margin.right + margin.left)
+          .attr('height', height + margin.top + margin.bottom);
+        chartWrapper.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+        //update the axis and line
+        xAxis.scale(x);
+        yAxis.scale(y).orient(window.innerWidth < breakPoint ? 'right' : 'left');
+        zAxis.scale(z).orient(window.innerWidth < breakPoint ? 'right' : 'left');
+
+        if(window.innerWidth < breakPoint) {
+            xAxis.ticks(d3.time.year, 4)
+        }
+        else {
+            xAxis.ticks(d3.time.year, 2)
+        }
+
+        svg.select('.x.axis')
+            .attr('transform', 'translate(0,' + height + ')')
+            .transition()
+            .duration(800)
+            .call(xAxis);
+
+        if (chart_id == 0){
+            svg.select('.y.axis')
+                .transition()
+                .duration(800)
+                .call(yAxis);
+            path.transition()
+                .duration(800)
+                .attr('d', line);
+        } else {
+            svg.select('.y.axis')
+                .transition()
+                .duration(800)
+                .call(zAxis);
+            path.transition()
+                .duration(800)
+                .attr('d', line2);
+        }
+
+        clearAuxillaryElements();
+
+        chartWrapper.selectAll("circle .dot")	
+            .data(data)			
+            .enter().append("circle")
+                .attr("class", "dot")								
+                .attr("r", 4)		
+                .attr("cx", function(d) { return x(new Date(d.date)) })		 
+                .attr("cy", function(d) { return scaleOrdinate(getOrdinateValue(d))})
+
+        chartWrapper.selectAll("circle .bubble")	
+            .data(data)			
+            .enter().append("circle")
+                .attr("class", "bubble")									
+                .attr("r", 15)		
+                .attr("cx", function(d) { return x(new Date(d.date)) })		 
+                .attr("cy", function(d) { return scaleOrdinate(getOrdinateValue(d))})
+                .style("opacity", 0)	
+                .on("mouseover", function(d) {		
+                    ordinate_label	
+                        .style("opacity", .9);		
+                    ordinate_label.html(d.date + ": <br>" + getOrdinateValue(d))	
+                        .style("left", (d3.event.pageX) + "px")		
+                        .style("top", (d3.event.pageY) + "px");	
+                    })					
+                .on("mouseout", function(d) {		
+                    ordinate_label	
+                        .style("opacity", 0);	
+                });
+        
+        d3.select(window).on("load",slideshow);
+        d3.select("#chart_id").on("change",changeChart);
+    }
+
+
+    function updateDimensions(winWidth) {
+        margin.top = 40;
+        margin.right = (winWidth < breakPoint) ? 20 : 80;
+        margin.left = (winWidth < breakPoint) ? 10 : 50;
+        margin.bottom = 50;
+
+        width = (winWidth > breakPoint) ? 750 - margin.left - margin.right : 350 - margin.left - margin.right;
+        height = 0.55*width;
+    }
+
+    function clearAuxillaryElements(){
+        locator.attr('display', 'none');
+        var dots = chartWrapper.selectAll(".dot");
+        dots.transition()
+        .duration(300)
+        .remove();
+        var bubbles = chartWrapper.selectAll(".bubbles");
+        bubbles.remove();
+    }
+
+    function identifyMouse(){
+        var YEAR_START = 1900;
+        var coordinates = d3.mouse(this);
+        var xPos = adjustYear(x.invert(coordinates[0]));
+        var pos = bisect(data, xPos);
+        locator
+            .attr('cx', x(new Date(data[pos].date)))
+            .attr('cy', scaleOrdinate(getOrdinateValueByPosition(pos)))
+            .attr('display', 'block');
+    }
+
+    function adjustYear(xPos){
+        var YEAR_START = 1900;
+        xPos = (xPos.getMonth() + 1 > 6) ? xPos.getYear() + 1 : xPos.getYear();
+        xPos += YEAR_START;
+        return xPos;
+    }
+
+    function getOrdinateValue(d){
+        if (chart_id == 0){
+            return d.value;
+        } else {
+            return d.value2;
+        }
+    }
+
+    function getOrdinateValueByPosition(pos){
+        if (chart_id == 0){
+            return data[pos].value;
+        } else {
+            return data[pos].value2;
+        }
+    }
+
+    function scaleOrdinate(ordinate_value){
+        if (chart_id == 0){
+            return y(ordinate_value);
+        } else {
+            return z(ordinate_value);
+        }
+    }
+
+    function userChangedChart(){
+        i = STOP_SLIDESHOW;
+        changeChart();
+    }
+
+    function changeChart(){
+        chart_id = d3.select("#chart_id").property('value');
+        render();
+    }
+
+    var i = 0;
+    var STOP_SLIDESHOW = 5;
+    function slideshow() { 
+        if (i < STOP_SLIDESHOW){
+            var id = i % 2;
+            d3.select("#chart_id").node().value = id.toString();
+            changeChart();
+            
+            ++i;
+            setTimeout(slideshow, 5000);
+        }      
+    }
+
+    return {
+        render : render
+    }
+  
+})(window,d3)
